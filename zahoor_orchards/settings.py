ae7ef76e178f701f73117353bca321192aa484b5
talhakhalidmtk <87082752+talhakhalidmtk@users.noreply.github.com>
2022-05-14 00:44:01 +0500

    Initial commit

--- zahoor_orchards/settings.py
+++ zahoor_orchards/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-a&6hxo7b1&wuz#z55^4-23to^#3#(%ei=)(ph&$*tv&70e2-!*'
+SECRET_KEY = 'django-insecure-a&6hxo7b1&wuz#z55^4-23to^#3#(%ei=)(ph&$*tv&70e2-!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
