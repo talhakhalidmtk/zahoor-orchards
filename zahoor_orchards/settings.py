5b48bc806422b1c8bec60ead1539e88664f42158
talhakhalidmtk <87082752+talhakhalidmtk@users.noreply.github.com>
2022-06-15 20:13:25 +0500

    Initial Commit 1

--- zahoor_orchards/settings.py
+++ zahoor_orchards/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
